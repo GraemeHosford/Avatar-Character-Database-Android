d486420094327afa7a3efd2ff761f663990fdf77
Graeme Hosford <graemehosford@gmail.com>
2020-07-09 16:48:37 +0100

    Created BaseActivity to handle any common Activity functionality

---
+++ app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/common/view/activity/BaseActivity.kt
@@ -0,0 +1,8 @@
+package graeme.hosford.avatarcharacterdatabase.ui.common.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Base class for handling common functionality in an Activity.
+ */
+abstract class BaseActivity : AppCompatActivity()

73b9246a8e91bc8b114782aaeebd799ebe546829
Graeme Hosford <graemehosford@gmail.com>
2020-07-09 15:21:01 +0100

    Added viewmodel dependencies. Created CharacterListViewModel.kt and change in ListUiModelProcessor.kt

---
+++ app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/character/list/view/CharacterListViewModel.kt
@@ -0,0 +1,31 @@
+package graeme.hosford.avatarcharacterdatabase.ui.character.list.view
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import graeme.hosford.avatarcharacterdatabase.repo.character.CharacterRepo
+import graeme.hosford.avatarcharacterdatabase.ui.character.list.model.CharacterListItemUiModel
+import graeme.hosford.avatarcharacterdatabase.ui.character.list.model.CharacterListItemUiModelProcessor
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+
+class CharacterListViewModel constructor(
+    private val characterRepo: CharacterRepo,
+    private val characterListItemUiModelProcessor: CharacterListItemUiModelProcessor
+) : ViewModel() {
+
+    private val charactersMutable = MutableLiveData<List<CharacterListItemUiModel>>()
+    val characters: LiveData<List<CharacterListItemUiModel>>
+        get() = charactersMutable
+
+    @ExperimentalCoroutinesApi
+    fun getCharacterList() {
+        viewModelScope.launch {
+            val characterEntities = characterRepo.getCharacterList()
+            val characterUiModels = characterListItemUiModelProcessor.process(characterEntities)
+            charactersMutable.value = characterUiModels
+        }
+    }
+
+}
--- app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/common/uimodel/ListUiModelProcessor.kt
+++ app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/common/uimodel/ListUiModelProcessor.kt
@@ -2,18 +2,21 @@ package graeme.hosford.avatarcharacterdatabase.ui.common.uimodel
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 
 abstract class ListUiModelProcessor<Entity, UiModel : BaseUiModel>(
     private val converter: UiModelConverter<Entity, UiModel>
 ) {
     @ExperimentalCoroutinesApi
-    fun process(entities: List<Entity>): Flow<UiModel> {
+    suspend fun process(entities: List<Entity>): List<UiModel> {
+        val models = arrayListOf<UiModel>()
         return entities.asFlow()
-            .flowOn(Dispatchers.Default)
-            .map { converter.toUiModel(it) }
+            .map {
+                converter.toUiModel(it)
+            }.flowOn(Dispatchers.Default)
+            .toList(models)
     }
 }

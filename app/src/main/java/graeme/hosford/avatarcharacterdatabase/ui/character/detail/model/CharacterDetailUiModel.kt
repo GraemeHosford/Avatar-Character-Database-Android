b0f7e368a7508f0239000ac396c42a4dac43eb0b
Graeme Hosford <graemehosford@gmail.com>
2020-07-11 12:54:00 +0100

    Created the UiModel CharacterSingleDetailUiModel to handle a character detail such as voicedBy or weapon etc. Updated CharacterDetailUiModel and associated converter to use this new class plus added string resources for it

--- app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/character/detail/model/CharacterDetailUiModel.kt
+++ app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/character/detail/model/CharacterDetailUiModel.kt
@@ -7,18 +7,7 @@ class CharacterDetailUiModel(
     id: Long,
     name: String,
     photoUrl: String?,
-    val gender: String?,
-    val eyeColour: String?,
-    val hairColour: String?,
-    val skinColour: String?,
-    val weapon: String?,
-    val loves: String?,
-    val profession: String?,
-    val position: String?,
-    val predecessor: String?,
-    val affiliation: String?,
-    val first: String?,
-    val voicedBy: String?
+    val details: List<CharacterSingleDetailUiModel>
 ) : CharacterUiModel(id, name, photoUrl) {
     override fun <UiModel : BaseUiModel> areContentsTheSame(other: UiModel): Boolean {
         if (other !is CharacterDetailUiModel) {
@@ -26,17 +15,6 @@ class CharacterDetailUiModel(
         }
 
         return super.areContentsTheSame(other)
-                && gender == other.gender
-                && eyeColour == other.eyeColour
-                && hairColour == other.hairColour
-                && skinColour == other.skinColour
-                && weapon == other.weapon
-                && loves == other.loves
-                && profession == other.profession
-                && position == other.position
-                && predecessor == other.predecessor
-                && affiliation == other.affiliation
-                && first == other.first
-                && voicedBy == other.voicedBy
+                && details == other.details
     }
 }
--- app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/character/detail/model/CharacterDetailUiModelConverter.kt
+++ app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/character/detail/model/CharacterDetailUiModelConverter.kt
@@ -1,5 +1,7 @@
 package graeme.hosford.avatarcharacterdatabase.ui.character.detail.model
 
+import androidx.annotation.StringRes
+import graeme.hosford.avatarcharacterdatabase.R
 import graeme.hosford.avatarcharacterdatabase.entity.CharacterEntity
 import graeme.hosford.avatarcharacterdatabase.ui.common.uimodel.UiModelConverter
 import javax.inject.Inject
@@ -11,18 +13,39 @@ class CharacterDetailUiModelConverter @Inject constructor() :
             id,
             characterName,
             photoUrl,
-            gender,
-            eyeColour,
-            hairColour,
-            skinColour,
-            weapon,
-            loves,
-            profession,
-            position,
-            predecessor,
-            affiliation,
-            first,
-            voicedBy
+            listOfNotNull(
+                /* Not getting an Id from database to use with these UiModels but a hardcoded value
+                * works for nested UiModels here. Just need to ensure they are each distinct and
+                * negative so they can't possibly overlap any database Id. This shouldn't be an
+                * issue in this app but could be in a screen which uses a RecyclerView with multiple
+                * different view types. */
+                getDetailUiModel(-1L, R.string.character_detail_gender, gender),
+                getDetailUiModel(-2L, R.string.character_detail_eye_colour, eyeColour),
+                getDetailUiModel(-3L, R.string.character_detail_hair_colour, hairColour),
+                getDetailUiModel(-4L, R.string.character_detail_skin_colour, skinColour),
+                getDetailUiModel(-5L, R.string.character_detail_weapon, weapon),
+                getDetailUiModel(-6L, R.string.character_detail_loves, loves),
+                getDetailUiModel(-7L, R.string.character_detail_profession, profession),
+                getDetailUiModel(-8L, R.string.character_detail_position, position),
+                getDetailUiModel(-9L, R.string.character_detail_predecessor, predecessor),
+                getDetailUiModel(-10L, R.string.character_detail_affiliation, affiliation),
+                getDetailUiModel(-11L, R.string.character_detail_first, first),
+                getDetailUiModel(-12L, R.string.character_detail_voiced_by, voicedBy)
+            )
         )
+    }
+
+    private fun getDetailUiModel(
+        modelId: Long,
+        @StringRes titleString: Int,
+        detail: String?
+    ): CharacterSingleDetailUiModel? {
+        return detail?.let {
+            CharacterSingleDetailUiModel(
+                modelId,
+                titleString,
+                detail
+            )
+        }
     }
 }

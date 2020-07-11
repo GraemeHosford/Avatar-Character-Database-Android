72737d0ff0a552ad7c3622059cc7f43504687189
Graeme Hosford <graemehosford@gmail.com>
2020-07-11 13:27:01 +0100

    Added a new layout for the character detail fragment and added logic for showing data on screen to the fragment itself

--- app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/character/detail/view/CharacterDetailFragment.kt
+++ app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/character/detail/view/CharacterDetailFragment.kt
@@ -1,14 +1,62 @@
 package graeme.hosford.avatarcharacterdatabase.ui.character.detail.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
-import graeme.hosford.avatarcharacterdatabase.ui.common.view.fragment.BaseFragment
+import graeme.hosford.avatarcharacterdatabase.databinding.FragmentCharacterDetailLayoutBinding
+import graeme.hosford.avatarcharacterdatabase.ui.character.detail.model.CharacterSingleDetailUiModel
+import graeme.hosford.avatarcharacterdatabase.ui.character.detail.view.adapter.CharacterDetailRecyclerViewAdapter
+import graeme.hosford.avatarcharacterdatabase.ui.character.detail.view.adapter.CharacterDetailViewHolder
+import graeme.hosford.avatarcharacterdatabase.ui.common.view.fragment.BaseRecyclerViewFragment
 
 @AndroidEntryPoint
-class CharacterDetailFragment : BaseFragment() {
+class CharacterDetailFragment :
+    BaseRecyclerViewFragment<
+            CharacterSingleDetailUiModel,
+            CharacterDetailViewHolder,
+            CharacterDetailRecyclerViewAdapter>() {
 
     private val viewmodel: CharacterDetailViewModel by viewModels()
     private val args: CharacterDetailFragmentArgs by navArgs()
 
+    private lateinit var binding: FragmentCharacterDetailLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewmodel.loadCharacterDetails(args.characterId)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCharacterDetailLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewmodel.characterData.observe(viewLifecycleOwner, Observer {
+            if (it.photoUrl.isNullOrBlank()) {
+                binding.characterDetailPicture.visibility = View.GONE
+            } else {
+                Glide.with(this)
+                    .load(it.photoUrl)
+                    .into(binding.characterDetailPicture)
+            }
+
+            recyclerViewAdapter.updateListItems(it.details)
+        })
+    }
+
+    override fun recyclerViewAdapter() = CharacterDetailRecyclerViewAdapter()
+
+    override fun getBaseRecyclerViewLayout() = binding.characterDetailBaseRecyclerViewLayout
 }

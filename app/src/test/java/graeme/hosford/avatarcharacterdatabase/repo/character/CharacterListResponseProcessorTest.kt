f0ca5cd9a3566d309b928989e5b5e8e687d26219
Graeme Hosford <graemehosford@gmail.com>
2020-07-12 18:28:39 +0100

    Created tests for some classes in repo.character package

---
+++ app/src/test/java/graeme/hosford/avatarcharacterdatabase/repo/character/CharacterListResponseProcessorTest.kt
@@ -0,0 +1,90 @@
+package graeme.hosford.avatarcharacterdatabase.repo.character
+
+import graeme.hosford.avatarcharacterdatabase.entity.CharacterEntity
+import graeme.hosford.avatarcharacterdatabase.network.character.CharacterResponse
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class CharacterListResponseProcessorTest {
+
+    private lateinit var processor: CharacterListResponseProcessor
+
+    @RelaxedMockK
+    private lateinit var converter: CharacterResponseConverter
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+
+        processor = CharacterListResponseProcessor(converter)
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun process_correctlyConvertsResponses_toEntities() = runBlocking {
+        val response = getResponse()
+        val responseList = listOf(response)
+
+        val expectedEntity = getCharacterEntity(0L, characterId = "TestId", name = "Aang")
+        val expectedEntityList = listOf(expectedEntity)
+
+        every { converter.toEntity(response) } returns expectedEntity
+
+        val entities = processor.process(responseList)
+
+        Assert.assertThat(entities, CoreMatchers.equalTo(expectedEntityList))
+    }
+
+    private fun getResponse(
+        characterId: String = "TestId",
+        characterName: String = "Aang"
+    ) = CharacterResponse(
+        characterId,
+        characterName,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+    )
+
+    private fun getCharacterEntity(
+        id: Long,
+        characterId: String = "FakeId",
+        name: String,
+        photoUrl: String? = null,
+        gender: String? = null,
+        eyeColour: String? = null,
+        hairColour: String? = null,
+        skinColour: String? = null,
+        weapon: String? = null,
+        loves: String? = null,
+        profession: String? = null,
+        position: String? = null,
+        predecessor: String? = null,
+        affiliation: String? = null,
+        first: String? = null,
+        voicedBy: String? = null
+    ) = CharacterEntity(
+        id, characterId, name, photoUrl, gender, eyeColour, hairColour, skinColour, weapon,
+        loves, profession, position, predecessor, affiliation, first, voicedBy
+    )
+
+}

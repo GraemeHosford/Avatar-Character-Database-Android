8c330c3ac3a7e7a619e0997142f5cab2f1c1e376
Graeme Hosford <graemehosford@gmail.com>
2020-07-12 18:04:37 +0100

    Created tests for classes in repo.common package

---
+++ app/src/test/java/graeme/hosford/avatarcharacterdatabase/repo/common/ListResponseProcessorTest.kt
@@ -0,0 +1,51 @@
+package graeme.hosford.avatarcharacterdatabase.repo.common
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class ListResponseProcessorTest {
+
+    private lateinit var processor: TestListResponseProcessor
+
+    @Before
+    fun setup() {
+        processor = TestListResponseProcessor()
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun process_correctlyConvertsResponses_toEntities() = runBlocking {
+        val responses = listOf(
+            TestResponse(1L, "Aang"),
+            TestResponse(2L, "Katara")
+        )
+
+        val expectedEntities = listOf(
+            TestEntity(1L, "Aang"),
+            TestEntity(2L, "Katara")
+        )
+
+        val entities = processor.process(responses)
+
+        assertThat(entities, equalTo(expectedEntities))
+    }
+
+    private data class TestResponse(val id: Long = 1L, val name: String = "Aang")
+    private data class TestEntity(val id: Long, val name: String)
+
+    private inner class TestResponseConverter : ResponseConverter<TestResponse, TestEntity> {
+        override fun toEntity(response: TestResponse) = with(response) {
+            TestEntity(
+                id,
+                name
+            )
+        }
+    }
+
+    private inner class TestListResponseProcessor :
+        ListResponseProcessor<TestResponse, TestEntity>(TestResponseConverter())
+}

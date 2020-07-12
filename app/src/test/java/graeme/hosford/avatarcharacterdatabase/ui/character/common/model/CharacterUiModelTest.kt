73919522e25e396e72a8d15445fc199291586e3b
Graeme Hosford <graemehosford@gmail.com>
2020-07-12 17:15:34 +0100

    Updated tests to call areContentsTheSame instead of equals

--- app/src/test/java/graeme/hosford/avatarcharacterdatabase/ui/character/common/model/CharacterUiModelTest.kt
+++ app/src/test/java/graeme/hosford/avatarcharacterdatabase/ui/character/common/model/CharacterUiModelTest.kt
@@ -7,26 +7,26 @@ import org.junit.Test
 class CharacterUiModelTest {
 
     @Test
-    fun characterUiModel_equals_returnsTrue_forModels_withSameValues() {
+    fun characterUiModel_areContentsTheSame_returnsTrue_forModels_withSameValues() {
         val model1 = TestCharacterUiModel()
         val model2 = TestCharacterUiModel()
 
-        assertTrue(model1 == model2)
+        assertTrue(model1.areContentsTheSame(model2))
     }
 
     @Test
-    fun characterUiModel_equals_returnsFalse_forModels_withAnyDifferingValue() {
+    fun characterUiModel_areContentsTheSmae_returnsFalse_forModels_withAnyDifferingValue() {
         val model1 = TestCharacterUiModel(id = 1L)
         val model2 = TestCharacterUiModel(id = 2L)
-        assertFalse(model1 == model2)
+        assertFalse(model1.areContentsTheSame(model2))
 
         val model3 = TestCharacterUiModel(name = "Katara")
         val model4 = TestCharacterUiModel(name = "Toph")
-        assertFalse(model3 == model4)
+        assertFalse(model3.areContentsTheSame(model4))
 
         val model5 = TestCharacterUiModel(photoUrl = "fakeUrl")
         val model6 = TestCharacterUiModel(photoUrl = "otherUrl")
-        assertFalse(model5 == model6)
+        assertFalse(model5.areContentsTheSame(model6))
     }
 
     private inner class TestCharacterUiModel(

a8b56d2e7c504b40bb62d1827860ee0399d40e28
Graeme Hosford <graemehosford@gmail.com>
2020-07-13 15:40:18 +0100

    Added comment explaining logic of function getCharacterList

--- app/src/main/java/graeme/hosford/avatarcharacterdatabase/repo/character/CharacterRepoImpl.kt
+++ app/src/main/java/graeme/hosford/avatarcharacterdatabase/repo/character/CharacterRepoImpl.kt
@@ -13,6 +13,19 @@ class CharacterRepoImpl @Inject constructor(
     private val characterListProcessor: CharacterListResponseProcessor,
     private val singleCharacterProcessor: SingleCharacterResponseProcessor
 ) : CharacterRepo {
+
+    /*
+    * The logic here avoids the network call when the database has values because the data on the
+    * server-side is not changing, and will not due to the show being long finished.
+    *
+    * This allows us to avoid the network call in some instances once the database is populated.
+    * This logic would not work if the server-side data was changing and would need to instead match
+    * the Flow logic in #getSingleCharacter below.
+    *
+    * Despite this the logic here may need to change in future anyway if I implement pagination.
+    * Right now the network call pulls the first 100 characters from the server when there are
+    * close to 500 there. This is a hardcoded limit right now to save on load times and data usage.
+    * */
     @ExperimentalCoroutinesApi
     override suspend fun getCharacterList(): List<CharacterEntity> {
         val characters = characterDao.getAllCharacters()

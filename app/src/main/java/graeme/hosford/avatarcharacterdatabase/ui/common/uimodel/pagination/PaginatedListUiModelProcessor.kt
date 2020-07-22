4583aa9f1616849e0ad3d1a39dea3493e12c3e5a
Graeme Hosford <graemehosford@gmail.com>
2020-07-22 15:26:26 +0100

    Made paginatedItems val private

--- app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/common/uimodel/pagination/PaginatedListUiModelProcessor.kt
+++ app/src/main/java/graeme/hosford/avatarcharacterdatabase/ui/common/uimodel/pagination/PaginatedListUiModelProcessor.kt
@@ -8,7 +8,7 @@ abstract class PaginatedListUiModelProcessor<Entity, UiModel : BaseUiModel>(
     converter: UiModelConverter<Entity, UiModel>,
     comparator: Comparator<UiModel>
 ) : ListUiModelProcessor<Entity, UiModel>(converter, comparator) {
-    val paginatedItems = arrayListOf<UiModel>()
+    private val paginatedItems = arrayListOf<UiModel>()
 
     override suspend fun process(entities: List<Entity>): List<UiModel> {
         paginatedItems.addAll(super.process(entities))
